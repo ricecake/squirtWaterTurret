0079860c0ab4702e94520fe962974dfd4fce3b02
ricecake <ricecake@greenstuff.io>
2025-04-21 11:49:45 -0400

    start of multi

--- dualStepperDPTStartWithRadar.ino
+++ dualStepperDPTStartWithRadar.ino
@@ -138,6 +138,9 @@ void initTestData() {
 	positions.push_back(MoveCmd(0, 0, maxSpeed, 2000));
 }
 
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
 void setup() {
 	Serial.begin(9600);
 
@@ -171,6 +174,27 @@ void setup() {
 
 	initTestData();
 
+	//create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+		Task1code,   /* Task function. */
+		"Task1",     /* name of task. */
+		10000,       /* Stack size of task */
+		NULL,        /* parameter of the task */
+		1,           /* priority of the task */
+		&Task1,      /* Task handle to keep track of created task */
+		0            /* pin task to core 0 */       
+	);
+
+	//create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(
+		Task2code,   /* Task function. */
+		"Task2",     /* name of task. */
+		10000,       /* Stack size of task */
+		NULL,        /* parameter of the task */
+		1,           /* priority of the task */
+		&Task2,      /* Task handle to keep track of created task */
+		1            /* pin task to core 0 */       
+	);
 
 	Serial.println("Ready!");
 
@@ -278,4 +302,26 @@ void loop() {
 		last = position;
 	}
 	steppers.run();
+}
+
+
+
+void Task1code( void * pvParameters ){
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;){
+    delay(1000);
+		Serial.println("Task 1");
+  } 
+}
+
+void Task2code( void * pvParameters ){
+  Serial.print("Task2 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;){
+    delay(700);
+		Serial.println("Task 2");
+  }
 }
